85f984d25782d748470f884a2f2a494f02e42641
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-03-03 13:51:20 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -34,7 +34,7 @@ body, p, span, div, label, h1, h2, h3, h4 {
     border-radius: 12px;
     height: 48px;
     font-size: 16px;
-    background-color: #3ef760;
+    background-color: #5cc8a1;
     color: white !important;
 }
 
