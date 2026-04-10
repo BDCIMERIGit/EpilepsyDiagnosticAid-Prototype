4ae4a6dca1dc5632b8431a6a4e06171e984a3ee8
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-04-10 16:10:32 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -44,7 +44,7 @@ section.main > div {
 
 /* BALIKIN WIDTH NORMAL */
 .block-container {
-    max-width: 35% !important;
+    max-width: 50% !important;
     transform: translateY(+25%);
     padding-top: 0rem;
 }
