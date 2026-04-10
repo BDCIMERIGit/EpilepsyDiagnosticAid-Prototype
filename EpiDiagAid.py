cab22a20cb3ca8ac4cb208afa6905aa9cf4c59fe
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-04-10 11:32:49 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -19,12 +19,12 @@ st.markdown(f"""
 <style>
 
 /* ================= BACKGROUND IMAGE ================= */
-.stApp [{
+.stApp {{
     background-image: url("data:image/png;base64,{bg_img}");
     background-size: 100% auto;
     background-repeat: no-repeat;
     background-position: top center;
-}]
+}}
 
 /* ================= MAIN CARD ================= */
 .main > div {{
