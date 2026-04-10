9be54e2420e5f4e9d669c3bd8e3c4ee6fab24b78
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-04-10 14:40:46 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -18,8 +18,8 @@ st.markdown(f"""
     background-image:
         linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
         url("data:image/png;base64,{bg_img}");
-    background-size: cover;
-    background-position: bottom center;
+    background-size: contain;
+    background-position: center;
     background-repeat: no-repeat;
     background-color: #0e1117;
 }}
