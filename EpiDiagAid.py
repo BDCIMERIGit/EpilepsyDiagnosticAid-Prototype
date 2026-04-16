ca3f1b33e693642680ada5e404665315af4a2c20
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-04-16 13:56:56 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -336,7 +336,7 @@ if st.session_state.show_welcome:
     /* ================= FIX BUTTON POSITION ================= */
     div.stButton {
         position: fixed;
-        top: 80%;
+        top: 78%;
         left: 77%;
         transform: translateX(-50%);
         width: 500px;
