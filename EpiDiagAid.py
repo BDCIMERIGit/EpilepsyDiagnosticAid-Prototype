365c8c34d66d8a4765fb9a8760b2cd30f67943a9
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-04-16 13:55:21 +0700

    Update EpiDiagAid.py

--- EpiDiagAid.py
+++ EpiDiagAid.py
@@ -356,11 +356,10 @@ if st.session_state.show_welcome:
     </style>
     """, unsafe_allow_html=True)
 
-    st.markdown("""
+    st.write("""
     <div class="welcome-container">
         <div class="welcome-box">
             <div class="welcome-title">Epilepsy Diagnostic Aid</div>
-
             <div class="welcome-text">
                 Epilepsi adalah diagnosis klinis, untuk itu diperlukan anamnesis yang baik dan terarah.
                 Aplikasi ini akan membantu para dokter dalam membuat diagnosis
